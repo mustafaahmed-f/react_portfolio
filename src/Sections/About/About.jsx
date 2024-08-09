67ac9de81e2d31de7de3b1f2f7550e13283a5edf
Mustafa Ahmed <113734819+mustafaahmed-f@users.noreply.github.com>
2024-08-09 15:30:17 +0300

    Update About.jsx

--- src/Sections/About/About.jsx
+++ src/Sections/About/About.jsx
@@ -63,7 +63,7 @@ function About() {
           <div className="flex items-center justify-start">
             <button className="px-6 py-2 text-lg text-white transition-all duration-500 border-transparent rounded-full cursor-pointer bg-secondaryColor hover:bg-hoverBtn">
               <a
-                href="https://drive.usercontent.google.com/u/0/uc?id=1xaohxGfDxo28UR9CWhTNWJtpw3E35PPO&export=download"
+                href="https://drive.usercontent.google.com/download?id=1jpuvpE-U-pOdab9Ky_IfsZV976seBGFn&export=download&authuser=0&confirm=t&uuid=d25ff679-8518-472d-b446-fcef4c341e40&at=APZUnTX7MhaNd_kRPl8RzMJQfrug:1723206551514"
                 className="text-white no-underline"
                 download={true}
                 target="_blank"
