aa8ba4e4e892514bb80eec6434fb74e5fb1a9702
Mustafa Ahmed <mostafafikry97@gmail.com>
2025-05-28 23:23:53 +0300

    Added ecommerce project to next.js section

--- src/Sections/Projects/NextJsProjects.js
+++ src/Sections/Projects/NextJsProjects.js
@@ -1,6 +1,19 @@
 export const NextJsProjects = [
   {
     id: "next1",
+    name: "E-Commerce",
+    imgUrl:
+      "https://res.cloudinary.com/dvvmu40wx/image/upload/v1747771299/Projects/Ecommerce_nextjs_sdb2m0.png",
+    category: "Next.js",
+    icon: "https://assets.vercel.com/image/upload/v1662130559/nextjs/Icon_dark_background.png",
+    liveLink: "https://ecommerce-nextjs-by-mustafa.vercel.app/",
+    githubLink: "https://github.com/mustafaahmed-f/ecommerce-nextjs",
+    description:
+      "A modern e-commerce platform built with Next.js, TypeScript, MongoDB, Redis, and Tailwind CSS, featuring advanced features like offline cart support and autocomplete search. The platform is built using modern web technologies and follows best practices for scalability and maintainability.",
+    technologies: ["Next.js", "Typescript", "MongoDB", "Redis", "TailwindCSS"],
+  },
+  {
+    id: "next2",
     name: "Wild Oasis",
     imgUrl:
       "https://res.cloudinary.com/dvvmu40wx/image/upload/v1730406543/Projects/Wil_oasis_dl4ix5.png",
