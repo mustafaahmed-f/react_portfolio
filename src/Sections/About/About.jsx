97a44d718029c0afcf375a0816f74e48d2ddc661
Mustafa Ahmed <mostafafikry97@gmail.com>
2025-07-26 23:11:20 +0300

    updated cv link

--- src/Sections/About/About.jsx
+++ src/Sections/About/About.jsx
@@ -68,7 +68,7 @@ function About() {
           <div className="flex items-center justify-start">
             <button className="px-6 py-2 text-lg text-white transition-all duration-500 border-transparent rounded-full cursor-pointer bg-secondaryColor hover:bg-hoverBtn">
               <a
-                href="https://drive.usercontent.google.com/download?id=1xVgK9-mbiVp398YcdZ16BN9vSkDl9R12&export=download&authuser=0&confirm=t&uuid=0aac53a1-3ae9-4e99-87d2-1fe9537aa397&at=APvzH3q2NN7_yd5e8Js810JIZWuV:1734704177916"
+                href="https://drive.google.com/file/d/1aJnO-iSMWZy1QWkGT-miy8r2I4vBwlGv/view?usp=sharing"
                 className="text-white no-underline"
                 download={true}
                 target="_blank"
